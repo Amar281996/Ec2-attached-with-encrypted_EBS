8b5ae8bd4766121d7de3cc8090379950011aa19a
Amar281996 <91563339+Amar281996@users.noreply.github.com>
2022-07-01 11:22:22 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
-# Output variable definitions
+# Output variable decalartion
+
 output "vpc_public_subnets" {
   description = "IDs of the VPC's public subnets"
   value       = module.vpc.public_subnets
